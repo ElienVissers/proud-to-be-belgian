49f0e386bf3e72f575e5eea5fe96b72c7ade502a
Elien Vissers <elien.visserssimilon@gmail.com>
2019-01-14 13:28:37 +0100

    petition part 2

--- db.js
+++ db.js
@@ -5,14 +5,24 @@ const db = spicedPg(`postgres:${dbUser}:${dbPassword}@localhost:5432/petition`);
 module.exports.addSignature = function(first, last, sig) {
     return db.query(
         `INSERT INTO signatures (first, last, sig)
-        VALUES ($1, $2, $3)`,
+        VALUES ($1, $2, $3)
+        RETURNING id`,
         [first, last, sig]
     );
 }
 
 module.exports.getSigners = function() {
     return db.query(
-        `SELECT first, last, sig
+        `SELECT first, last
         FROM signatures`
+    );
+}
+
+module.exports.getSignature = function(id) {
+    return db.query(
+        `SELECT sig
+        FROM signatures
+        WHERE id = $1`,
+        [id]
     );
 }
--- index.js
+++ index.js
@@ -2,46 +2,78 @@ const express = require('express');
 const hb = require('express-handlebars');
 const app = express();
 const db = require('./db');
+const cookieSession = require('cookie-session');
 
 app.engine('handlebars', hb());
 app.set('view engine', 'handlebars');
 app.use(require('body-parser').urlencoded({extended: false}));
+app.use(cookieSession({
+    secret: `I'm always angry.`,
+    maxAge: 1000 * 60 * 60 * 24 * 14
+}));
+
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/petition', (req, res) => {
-    res.render('mainTemplate', {
-        layout: 'main'
-    });
+    if (!req.session.signatureId) {
+        res.render('mainTemplate', {
+            layout: 'main'
+        });
+    } else {
+        res.redirect('/thankyou');
+    }
 });
 
 app.post('/petition', (req, res) => {
-    console.log("req.body: ", req.body);
-    db.addSignature(req.body.first, req.body.last, req.body.sig).then(function() {
+    //console.log("req.body: ", req.body);
+    db.addSignature(req.body.first, req.body.last, req.body.sig).then(function(sigId) {
+        console.log('sigId: ', sigId);
+        req.session.signatureId = sigId;
         res.redirect('/thankyou');
     }).catch(function(err) {
          res.render('mainTemplate', {
             error: true,
             layout: 'main'
-        })
+        });
     });
 });
 
 app.get('/thankyou', (req, res) => {
-    res.render('thankyouTemplate', {
-        layout: 'main'
-    });
+    //console.log("sig id in object :", req.session.signatureId.rows[0].id);
+    if (req.session.signatureId) {
+        db.getSignature(req.session.signatureId.rows[0].id).then(function(sig) {
+            console.log("then: ", sig);
+            res.render('thankyouTemplate', {
+                signature: sig.rows[0].sig,
+                layout: 'main'
+            });
+        }).catch(function(err) {
+            console.log(err);
+        });
+    } else {
+        res.redirect('/petition');
+    }
+
 });
 
 app.get('/signers', (req, res) => {
-    db.getSigners().then(function(signers) {
-        console.log("signers.rows: ", signers.rows)
-        res.render('signersTemplate', {
-            signers: signers.rows,
-            layout: 'main'
+    if (req.session.signatureId) {
+        db.getSigners().then(function(signers) {
+            res.render('signersTemplate', {
+                signers: signers.rows,
+                layout: 'main'
+            });
         });
-        //why is sig not showing in the console of singers.rows?
-    });
+    } else {
+        res.redirect('/petition');
+    }
+
+});
+
+app.get('/logout', (req, res) => {
+    req.session = null;
+    res.redirect('/petition');
 });
 
 app.listen(8080, () => console.log('Listening!'));
